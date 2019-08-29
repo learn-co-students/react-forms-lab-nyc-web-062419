1c3258cba324d07be0e697747129a7605b14a3fc
KConlon13 <KyleConlon13@gmail.com>
2019-08-29 14:07:00 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,7 +18,7 @@ class LoginForm extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
-    !this.state.username || !this.state.password ? null : this.props.handleLogin(this.state.username, this.state.password)
+    !this.state.username || !this.state.password ? null : this.props.handleLogin(this.state)
   }
 
 
